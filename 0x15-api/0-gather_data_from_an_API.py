b16f2a0e2a8773dda7a70c64ac8eae3247b46112
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-04-25 18:28:15 -0700

    fixed error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""export data in the JSON format.
-   The module sends a request to an API
-"""
-import json
+"""Returns information about his/her TODO list progress.
+   The module Takes in a user id, sends a request to an API
+    and displays the response
+ """
 import requests
 import sys
 
@@ -12,24 +12,26 @@ URL = "https://jsonplaceholder.typicode.com"
 
 
 if __name__ == "__main__":
-    users = requests.get("{}/users/".format(URL))
-    todos = requests.get("{}/todos/".format(URL))
-    if (users.status_code == requests.codes.ok and
-            todos.status_code == requests.codes.ok):
-        file_name = "todo_all_employees.json"
-        all_user_todo = {}
-        for user in users.json():
-            userid = user.get('id')
-            username = user.get('username')
-            user_todos = []
+    if (len(sys.argv) > 1 and
+            isinstance(int(sys.argv[1]), int)):
+        user_id = int(sys.argv[1])
+        user = requests.get("{}/users/{}".format(URL, user_id))
+        todos = requests.get("{}/user/{}/todos/".format(URL, user_id))
+        if (user.status_code == requests.codes.ok and
+                todos.status_code == requests.codes.ok):
+            username = user.json().get('name')
+            done_tasks = []
+            c_tasks = 0
+            tasks = 0
             for todo in todos.json():
-                if userid == todo.get('userId'):
-                    task = {}
-                    task['username'] = username
-                    task['task'] = todo.get('title')
-                    task['completed'] = todo.get('completed')
-                    user_todos.append(task)
-                    all_user_todo[userid] = user_todos
-
-        with open(file_name, 'w') as fs:
-            json.dump(all_user_todo, fs)
+                task_status = todo.get('completed')
+                if task_status is True:
+                    c_tasks = c_tasks + 1
+                    done_tasks.append(todo)
+                tasks = tasks + 1
+            print("Employee {} is done with tasks({}/{}):".format(
+                   username,
+                   c_tasks,
+                   tasks))
+            for todo in done_tasks:
+                print("\t {}".format(todo.get('title')))
