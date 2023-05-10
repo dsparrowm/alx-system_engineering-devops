7848fc448e8d979e79c382421b481d982af34433
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-05-10 13:48:35 -0700

    added task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""prints all hot posts recursively"""
+import requests
+import sys
+
+
+def recurse(subreddit, last_post=None, posts=None):
+    """ Make a GET request to the subreddit's hot.json endpoint"""
+    sub = sys.argv[1]
+    api = "https://www.reddit.com/r/{}/hot.json".format(sub)
+    response = requests.get(api,
+                            headers={"User-agent": "Mozilla/5.0"},
+                            params={"after": last_post})
+    # Check if the request was successful
+    if response.status_code != 200:
+        # If the request was not successful, return None
+        return None
+    # If the request was successful, parse the JSON response
+    data = response.json()
+    # Initialize the list of posts if it's not already created
+    if posts is None:
+        posts = []
+        # Extract the titles of the posts from the JSON data
+        for post in data["data"]["children"]:
+            posts.append(post["data"]["title"])
+            # Check if there are more pages to load
+            if data["data"]["after"] is not None:
+                """ If there are more pages to load, recursively
+                    call the function with the last post's name
+                    as the "after" parameter
+                """
+                recurse(subreddit, last_post=data["data"]["after"],
+                        posts=posts)
+                # Return the list of posts
+                return posts
