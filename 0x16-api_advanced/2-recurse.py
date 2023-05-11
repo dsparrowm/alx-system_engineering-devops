ddea35ba5f58ca30d36a6d8302bbfcec4912d248
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-05-10 17:12:28 -0700

    added task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit, the function should return None.
+"""queries the Reddit API and returns a list containing the titles
+   of all hot articles for a given subreddit.
+   If no results are found for the given subreddit,
+   the function should return None.
 """
 import requests
-import sys
 
 
-def recurse(subreddit, last_post=None, posts=None):
+def recurse(subreddit, next_page='', count=0, hot_list=[]):
     """ Make a GET request to the subreddit's hot.json endpoint"""
-    sub = sys.argv[1]
-    api = "https://www.reddit.com/r/{}/hot.json".format(sub)
-    response = requests.get(api,
-                            headers={"User-agent": "Mozilla/5.0"},
-                            params={"after": last_post})
-    # Check if the request was successful
-    if response.status_code != 200:
-        # If the request was not successful, return None
-        return None
-    # If the request was successful, parse the JSON response
-    data = response.json()
-    # Initialize the list of posts if it's not already created
-    if posts is None:
-        posts = []
-        # Extract the titles of the posts from the JSON data
-        for post in data["data"]["children"]:
-            posts.append(post["data"]["title"])
-            # Check if there are more pages to load
-            if data["data"]["after"] is not None:
-                """ If there are more pages to load, recursively
-                    call the function with the last post's name
-                    as the "after" parameter
-                """
-                recurse(subreddit, last_post=data["data"]["after"],
-                        posts=posts)
-                # Return the list of posts
-                return posts
+    req_params = {
+            'after': next_page,
+            'count': count,
+            'limit': 100
+            }
+    url = "{}/{}/hot.json".format(API, subreddit)
+
+    resp = requests.get(url, params=req_params,
+                        headers={"User-agent": "Mozilla/5.0"},
+                        allow_redirects=False)
+
+    # Check if request was successfull
+    if resp.status_code != requests.codes.ok:
+        return (None)
+
+    # Parse res to JSON and return list of all titles
+    listing = resp.json().get('data')
+    next_page = listing.get('after')  # Get the next page to fetch
+    count += len(listing.get('children'))
+
+    # Append all page titles to hot_list
+    for child in listing.get('children'):
+        hot_list.append(child.get('data').get('title'))
+
+    # Return hot list if no next page
+    if next_page is not None:
+        return recurse(subreddit, hot_list, next_page, count)
+    return hot_list
