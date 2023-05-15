a4defec54e82d9690e5c4742d90d6f927cb2537d
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-05-15 12:13:18 -0700

    added README, task 0 and task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# This manuscript enables the user to lig in
+
+# Increase hard file limit
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit 
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
